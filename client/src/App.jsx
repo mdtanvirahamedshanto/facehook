82b50a7a92c21181a401bc4498e8d5ab47581c34
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-03-04 15:47:49 +0600

    Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import NotFoundPage from "./components/pages/NotFoundPage";
+import ProfilePage from "./components/pages/ProfilePage";
+import RegistrationPage from "./components/pages/RegistrationPage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} exact />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
